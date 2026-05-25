682b07ecaed6cc4927046d8f6e0f725c5c3e77c9
maxcaplan <maxcaplan@gmail.com>
2026-05-25 19:24:49 -0300

    Configured mini statusline

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -8,3 +8,15 @@ if not MiniIcons then
 	require('mini.icons').setup()
 	MiniIcons.mock_nvim_web_devicons()
 end
+
+local statusline = require('mini.statusline')
+
+statusline.setup({
+	use_icons = vim.g.have_nerd_font,
+})
+
+-- Set status line location section to LINE:COLUMN
+---@diagnostic disable-next-line: duplicate-set-field
+statusline.section_location = function()
+	return '%2l:%-2v'
+end
