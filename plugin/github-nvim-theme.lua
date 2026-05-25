c623a14ba54f36edf4e6cac58300a11bb77b0a99
maxcaplan <maxcaplan@gmail.com>
2026-05-25 16:25:17 -0300

    Added github editor theme

---
+++ plugin/github-nvim-theme.lua
@@ -0,0 +1,9 @@
+-- Editor colorschemes
+
+vim.pack.add({
+	{ src = 'https://github.com/projekt0n/github-nvim-theme' },
+})
+
+require('github-theme').setup()
+
+vim.cmd.colorscheme('github_dark_default')
